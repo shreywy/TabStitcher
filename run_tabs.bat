060693e273dd90cc88f171b04ccf5c7f418ee441
shreywy <shreywy@users.noreply.github.com>
2025-08-16 22:14:08 -0400

    Update run_tabs.bat

--- run_tabs.bat
+++ run_tabs.bat
@@ -1,7 +1,13 @@
 @echo off
 title Tab Extractor
-:loop
+
+echo Updating Python and required packages... (may take a minute on first start)
+python -m pip install --upgrade pip >nul 2>&1
+python -m pip install -r requirements.txt >nul 2>&1
+echo Requirements installed/verified.
 echo.
+
+:loop
 set /p vidurl=Paste YouTube link (or type EXIT to quit): 
 
 if /i "%vidurl%"=="exit" goto end
@@ -16,3 +22,4 @@ goto loop
 
 :end
 echo Exiting...
+pause
